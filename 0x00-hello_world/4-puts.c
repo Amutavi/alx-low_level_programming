8220b2647043c91a0de6ea8285788ecc8770da75
Amutavi <amutaviess@gmail.com>
2022-10-18 06:08:39 -0700

    prepro

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - print the strings that inputs the function
+ *Description:printing "Programming is like building a multilingual puzzle"
+ *Return:0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingu\
+
+al puzzle");
+  return (0);
+}
