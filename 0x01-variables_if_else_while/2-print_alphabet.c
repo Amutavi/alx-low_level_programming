a52aeab399dc6d2b263e87a6867982f042a4866a
Amutavi <amutaviess@gmail.com>
2022-10-19 06:17:39 -0700

    Variables if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - outputs alphabet
+*
+* return: Always (0) succcess
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
