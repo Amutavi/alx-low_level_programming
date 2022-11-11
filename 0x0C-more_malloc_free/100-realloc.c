bbbe858e52d22ca7f55f235e90fd0ecf3c76f8bf
Amutavi <amutaviess@gmail.com>
2022-11-11 08:32:10 -0800

    Malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *realloc;
+	char *clone, *relloc;
 	unsigned int i;
 
 	if (ptr != NULL)
@@ -20,13 +20,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (new_size == 0 && ptr != NULL)
 		{ free(ptr);
 			return (0); }
-		realloc = malloc(new_size);
-		if (realloc == NULL)
+		relloc = malloc(new_size);
+		if (relloc == NULL)
 			return (0);
-		for (i = 0, i < (old_size || i < new_size); i++)
+		for (i = 0, i < (old_size || i < new_size); i++; )
 		{
-			*(realloc + i) = clone[i];
+			*(relloc + i) = clone[i];
 		}
 		free(ptr);
-		return (realloc);
+		return (relloc);
 		}
